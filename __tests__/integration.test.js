cb1139c0d2141e727ef75554b3aaee096286c607
heniscoding <heniscoding@Ubuntu-22.04.3-Desktop.myguest.virtualbox.org>
2024-02-19 11:39:47 +0000

    Completed task 2 - CORE: GET /api/topics

---
+++ __tests__/integration.test.js
@@ -0,0 +1,30 @@
+const app = require("../app");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const testData = require("../db/data/test-data/index");
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("getTopics", () => {
+  it("should return a 200 status code", () => {
+    return request(app).get("/api/topics").expect(200);
+  });
+  it("should return an array of topic objects, each of which should have the following properties: slug, description", () => {
+    return request(app)
+      .get("/api/topics")
+      .then((res) => {
+        expect(res.body.topics).toHaveLength(3);
+        res.body.topics.forEach((topic, index) => {
+          expect(typeof res.body.topics[index]).toBe("object");
+          expect(topic).hasOwnProperty("slug");
+          expect(topic).hasOwnProperty("description");
+        });
+      });
+  });
+});
